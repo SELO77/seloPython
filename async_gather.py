97a74de96fd0cad03d3c1f453790de528119965a
SELO77 <rochan87@gmail.com>
2016-07-25 14:45:39 +0900

    EX async_gather.py .

--- async_gather.py
+++ async_gather.py
@@ -1,69 +1,65 @@
+# asyncio Sample for MyMusicTaste
+# SELO77
+# 2016-07-24
 import asyncio
 import traceback
 
 
-class SpwanClass():
+class SpawnClass():
+
   def __init__(self):
-    print("SpwanClass.__init()__")
+    pass
 
   @asyncio.coroutine
   def execute(self, request_class_list):
-    print("SpwanClass.execute()")
     response = []
-
-    if request_class_list:
-      @asyncio.coroutine
-      def run(self, request_class):
-        try:
-          result = yield from request_class.execute()
-          response.append(result)
-        except:
-          print(traceback.format_exc())
-
-      try:
+    try:
+      if request_class_list:
+        @asyncio.coroutine
+        def run(request_class):
+          try:
+            result = yield from request_class.execute()
+            response.append(result)
+          except:
+            print(traceback.format_exc())
         tasks_list = []
         for each_class in request_class_list:
-          print(each_class)
           tasks_list.append(
             asyncio.Task(run(each_class))
           )
-
-        gather_result = yield from asyncio.gather(tasks_list)
-        print("gather_result:\n%s"%gather_result)
-
-      except:
+        yield from asyncio.gather(*tasks_list)
+    except:
         print(traceback.format_exc())
-
     return response
 
 
 class RequestClass1():
+
   def __init__(self):
     print('RequestClass1.__init()__')
 
   @asyncio.coroutine
   def execute(self):
-    print("RequestClass1.execute()")
+    return "class1_response"
 
 
 class RequestClass2():
+
   def __init__(self):
     print('RequestClass2.__init()__')
 
   @asyncio.coroutine
   def execute(self):
-    print("RequestClass2.execute()")
+    return "class2_response"
+
 
 @asyncio.coroutine
 def main():
   try:
     request_class_list = [RequestClass1(), RequestClass2()]
-    # RequestClass1()
-    # RequestClass2()
-
-    spwanClass = SpwanClass()
-    final_result = yield from spwanClass.execute(request_class_list)
-    print(final_result)
+    spawnClass = SpawnClass()
+    final_result = yield from spawnClass.execute(request_class_list)
+    print("final_result:%s"%final_result)
   except:
     print(traceback.format_exc())
 
