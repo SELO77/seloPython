3b50c3a1509599e1b06ba8c28c9343e660500d20
SELO77 <rochan87@gmail.com>
2016-08-29 00:37:22 +0900

    EX-Algorithm-Selection sort

---
+++ algorithm/selectionsort.py
@@ -0,0 +1,26 @@
+def selectionsort(list):
+    minimum = {
+                "index": None,
+                "value": None,
+            }
+    index = 0
+    for v in range(0, len(list)):
+        for e in range(index, len(list)):
+            if e == index:
+                minimum['index'] = e
+                minimum['value'] = list[e]
+                continue
+            if minimum['value'] > list[e]:
+                minimum['index'] = e
+                minimum['value'] = list[e]
+        temp = list[v]
+        list[v] = minimum['value']
+        list[minimum['index']] = temp
+        print list
+        index += 1
+    return list
+
+if __name__ == "__main__":
+    test_list = [35, 80, 21, 53, 11, 45, 92, 39]
+    result = selectionsort(test_list)
+    print result
